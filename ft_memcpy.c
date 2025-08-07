05ab8ea41d66667eb2609e858b77d87e9d09dbf1
forzen03 <noorjaradat2@gmail.com>
2025-08-08 01:51:12 +0300

    download my missing fucntions :(

---
+++ ft_memcpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: noorjaradat <noorjaradat@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/08 00:48:46 by noorjaradat       #+#    #+#             */
+/*   Updated: 2025/08/08 00:58:44 by noorjaradat      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+    unsigned char *d;
+    unsigned char *s;
+    size_t i;
+
+    s = (unsigned char *)src;
+    d = (unsigned char *)dest;
+    i = 0;
+    while(i < n)
+    {
+        d[i] = s[i];
+        i++;
+    }
+    return (dest);
+}
---
+++ ft_strlcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: noorjaradat <noorjaradat@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/08 01:20:22 by noorjaradat       #+#    #+#             */
+/*   Updated: 2025/08/08 01:39:01 by noorjaradat      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    if(size == 0)
+        return(ft_strlen(src));
+    while (src[i] && dst[i] && i < size - 1)
+    {
+        dst[i] = src[i];
+        i++;
+    }
+    dst[i] = '\0';
+    return (ft_strlen(src));
+}
